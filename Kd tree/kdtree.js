183e4f882c246f3785e0919c388a316fd44c5096
rogersisci <rogersisci@gmail.com>
2022-09-11 21:55:53 -0500

    Create kdtree.js

--- Kd tree/kdtree.js
+++ Kd tree/kdtree.js
@@ -9,16 +9,38 @@ class Node{
     }
 }
 
-function getHeight(node) {
-    if (node === null){
-        return 0;
-    }
-    // find the height of each subtree
-    var lh = getHeight(node.left);
-    var rh = getHeight(node.right);
-    return 1 + max(lh,rh);
-}
 
+// // class N_Point {
+// //     constructor(points) {
+// //         this.vectorialSpace = points;
+// //     }
+// // }
+
+// function getHeight(node) {
+//     if (node === null){
+//         return 0;
+//     }
+//     // find the height of each subtree
+//     var lh = getHeight(node.left);
+//     var rh = getHeight(node.right);
+//     return 1 + max(lh,rh);
+// }
+// function generate_dot(node){
+//     if (node === null){
+//         return "";
+//     }
+//     var tmp = '';
+//     if (node.left != null){
+//         tmp += '"' + node.point.toString() + '"' + ' -> ' + '"' + node.left.point.toString() + '"' + ';\n';
+//         tmp += generate_dot(node.left);
+//     }
+//     if (node.right != null){
+//         tmp += '"' + node.point.toString() + '"' + ' -> ' + '"' + node.right.point.toString() + '"' + ';\n';
+//         tmp += generate_dot(node.right);
+//     }
+//     //console.log(tmp);
+//     return tmp;
+// }
 
 function build_kdtree(points, depth = 0){
     var n = points.length;
@@ -33,8 +55,7 @@ function build_kdtree(points, depth = 0){
     }
     
     var median = Math.floor(points.length / 2);
-    
-    // sort by the axis :: ordenamos por el eje axis
+   
     points.sort(function(a, b)
     {
     return a[axis] - b[axis];
@@ -45,6 +66,7 @@ function build_kdtree(points, depth = 0){
     var right = points.slice(median + 1);
     
    // console.log(right);
+    
     var node = new Node(points[median].slice(0, k), axis);
     node.left = build_kdtree(left, depth + 1);
     node.right = build_kdtree(right, depth + 1);
@@ -52,13 +74,13 @@ function build_kdtree(points, depth = 0){
     return node;
 }
 
-
 function distanceSquared ( point1 , point2 ){
 	var distance = 0;
 	for (var i = 0; i < k; i ++)
 	distance += Math.pow (( point1 [i] - point2 [i]) , 2) ;
 	return Math.sqrt ( distance );
 }
+//function generate_dot(node){}
 
 
 function closest_point_brute_force(points, point){
@@ -67,7 +89,7 @@ function closest_point_brute_force(points, point){
     var best_point = null;
     for(let i = 0; i < points.length; i++){
         distance = distanceSquared(points[i], point);
-        console.log('('+points[i]+') = '+distance);
+        console.log(points[i]+' = '+distance);
         if(best_distance === null || distance < best_distance){
             best_distance = distance;
             //best_point = { 'point': points[i], 'distance': distance }
@@ -78,16 +100,47 @@ function closest_point_brute_force(points, point){
 }
 
 
+function naive_closest_point(node , point , depth = 0, best = null ) {
+    if(node != null){
+        if(depth == 0){
+            best = node.point.vectorialSpace;
+        }
+       
+        var axisDistance = point[node.axis] - node.point.vectorialSpace[node.axis]; 
+        let distanceBest = distanceSquared(best, point);
+        let distanceNode = distanceSquared(node.point.vectorialSpace,point);
+
+
+        if ( Math.abs(axisDistance) <= distanceBest ){
+            if(distanceBest > distanceNode){
+                best = node.point.vectorialSpace;
+            }
+            if(axisDistance > 0){
+                return naive_closest_point (node.right , point , depth+1, best );
+            }
+            else{
+                return naive_closest_point (node.left , point , depth+1, best);
+            }
+        }else{
+            return best;
+        }
+    }else{
+        return best;
+    }
+}
+
+/////////////////////////////////////////////mejorado
 scannedNodes = []; 
 bestDist = Infinity;
 function closest_point(node,point) {
-    //console.log('pc2');
+
     if (node === null) return;
     //console.log(node);
     //console.log(point);
     scannedNodes.push(node);
     var nodeDist = distanceSquared(node.point, point);
-
+    //console.log(nodeDist);
+    //console.log("-------------------");
     if (nodeDist < bestDist) {
         bestDist = nodeDist;
         guess = node;
@@ -111,7 +164,6 @@ function closest_point(node,point) {
             closest_point(otherNode,point);
         } 
     }
-    //console.log('pc3');
     return { 
         node: guess, 
         distance: bestDist,
@@ -119,42 +171,7 @@ function closest_point(node,point) {
     };
 } 
 
-scannedNodes2 = []; 
-bestDist2 = Infinity;
-function naive_closest_point(node,point) {
-    //console.log('pc2');
-    if (node === null) return;
-    //console.log(node);
-    //console.log(point);
-    scannedNodes2.push(node);
-    var nodeDist = distanceSquared(node.point, point);
-    //console.log(nodeDist);
-    //console.log("-------------------");
-    if (nodeDist < bestDist2) {
-        bestDist2 = nodeDist;
-        guess = node;
-        
-    }
-    console.log(node.point+' = ' +nodeDist);
-    // Busca de forma recursiva la mitad del árbol que contiene el objetivo
-    var side = point[node.axis] < node.point[node.axis] ? "left" : "right";
-    if (side == "left") {
-        naive_closest_point(node.left,point);
-        var otherNode = node.right
-    } else {
-        naive_closest_point(node.right,point);
-        var otherNode = node.left;
-    }  
-
-    return { 
-        node: guess, 
-        distance: bestDist2,
-        scannedNodes2: scannedNodes2
-    };
-} 
-
 function generate_dot(node) { 
-    //console.log('prueba dop');
     var cad = '';
 	if(node == null)
 		return '';
@@ -239,12 +256,12 @@ BPQ.prototype.add = function(value, priority) {
 };  
 
 function range_query_rect(node, center, hug, queue, depth = 0) {
-    //console.log('prueba2');
+    console.log('prueba2');
 	if (node == null) {
 		return null;
 	}
 	p += 1;
-	//console.log(p);
+	console.log(p);
 	var axis = node.axis;
 	var next_branch = null;
 	var opposite_branch = null;
@@ -255,58 +272,26 @@ function range_query_rect(node, center, hug, queue, depth = 0) {
 		next_branch = node.right;
 		opposite_branch = node.left;
 	}
-    //console.log(hug[0]);
 	var best = closer_point(center, node, range_query_rect(next_branch, center, hug, queue, depth + 1));
 	if (Math.abs(center[axis] - node.point[axis]) <= hug[axis] * 2 || distanceSquared(center, best.point) > Math.abs(center[axis] - node.point[axis])) {
 		if (Math.abs(center[0] - node.point[0]) <= hug[0] && Math.abs(center[1] - node.point[1]) <= hug[1]) {
 			queue.push(node.point);
-            console.log(node.point);
 		}
 		best = closer_point(center, best, range_query_rect(opposite_branch, center, hug, queue, depth + 1));
 	}
 	return best;
 }
-function closer_point(point, p1, p2) {
-    //console.log('1'+p1.point);
-    //console.log(point);
-    if (p2 == null) {
-		return p1;
-	}
-	var distance = distanceSquared(p1.point, point);
-	if (distance < distanceSquared(p2.point, point))
-		return p1;
-	return p2;
-}
 
-var p = 0;
-function range_query_circle(node, center, radio, queue, depth = 0) {
-	if (node == null) {
-		return null;
-	}
-	p += 1;
-	//console.log(p);
-	var axis = node.axis;
-	var next_branch = null;
-	var opposite_branch = null;
-	if (center[axis] < node.point[axis]) {
-		next_branch = node.left;
-		opposite_branch = node.right;
-
-	} else {
-		next_branch = node.right;
-		opposite_branch = node.left;
-	}
-	var best = closer_point(center, node, range_query_circle(next_branch, center, radio, queue, depth + 1));
-	if (Math.abs(center[axis] - node.point[axis]) <= radio || distanceSquared(center, best.point) > Math.abs(center[axis] - node.point[axis])) {
-		if (distanceSquared(center, node.point) <= radio) {
-			queue.push(node.point);
-            console.log(node.point);
-		}
-		best = closer_point(center, best, range_query_circle(opposite_branch, center, radio, queue, depth + 1));
-    }
-
-	return best;
-}
+// function closer_point(point, p1, p2) {
+//     console.log('prueba3');
+//     if (p2 == null) {
+// 		return p1;
+// 	}
+// 	var distance = distanceSquared(p1.point, point);
+// 	if (distance < distanceSquared(p2.point, point))
+// 		return p1;
+// 	return p2;
+// }
 // function getHeight(node){
 //     return height;
 // }
