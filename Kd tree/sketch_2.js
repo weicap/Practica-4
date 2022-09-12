b0e5ab31aaa4c89905a8e1b5f99cc9798adad6b7
rogersisci <rogersisci@gmail.com>
2022-09-11 21:54:41 -0500

    Update sketch_2.js

--- Kd tree/sketch_2.js
+++ Kd tree/sketch_2.js
@@ -1,33 +1,4 @@
-// function setup(){
-//     var width =250;
-//     var height = 200;
-//     createCanvas(width,height);
-
-//     background(0);
-//     for(var x=0;x<width;x+= width/10){
-//         for(var y=0;y<height;y+=height/5){
-//             stroke(125,125,125);
-//             strockWeight(1);
-//             line(x,0,x,height);
-//             line(0,y,width,y);
-//         }
-//     }
-
-//     var data = [];
-//     for(let i=0;i<12;i++){
-//         var x = Math.floor ( Math.random () * height );
-//         var y = Math.floor ( Math.random () * height );
-//         data.push ([x, y]) ;
-
-//         fill (255 , 255 , 255) ;
-//         circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
-//         textSize(8);
-//         text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
-//     }
-
-//     //var root = kdtree.build_kdtree(data);
-//     console.log("hola mundo");
-   
+  
 var root;
 var pointP = [140 ,90];
 var data=[];
